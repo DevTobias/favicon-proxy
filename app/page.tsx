c841cde78ae02598631130fb45878fc2703d4196
Tobias Kärst <tobi.kaerst@gmx.de>
2024-03-03 00:10:15 +0100

    feat: added test favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
 export default function Page() {
-  return <div>Favicon Proxy</div>;
+  return (
+    <>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="https://anytype.io/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="https://anytype.io/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="https://anytype.io/favicon-16x16.png"
+      ></link>
+      <div>Favicon Proxy</div>
+    </>
+  );
 }
